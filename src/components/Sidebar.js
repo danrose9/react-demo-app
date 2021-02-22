8015f4154be1736861b3d4ab2c0dddfef4535145
Dan Rose <dan@reddog32.com>
2021-02-22 11:40:57 +0000

    Move styled components from Sideabr to styles directory

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,37 +1,15 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import { SidebarData } from './SidebarData';
 import './Sidebar.css';
 import { IconContext } from 'react-icons';
-
-const StyledSidebar = styled.div`
-  background-color: #4f4a41;
-  overflow: auto;
-  height: auto;
-  padding: 0.5rem;
-  -webkit-overflow-scrolling: touch;
-  -ms-overflow-style: none;
-  width: 50px;
-`;
-
-const ToggleButton = styled.button`
-  position: absolute;
-  bottom: 150px;
-`;
-
-const StyledMenuItem = styled.ul`
-  width: 100%;
-  padding: 1px;
-`;
-
-const StyledSpan = styled.span`
-  margin-left: 20px;
-`;
-
-const StyledList = styled.li`
-  cursor: pointer;
-`;
+import {
+  StyledSidebar,
+  ToggleButton,
+  StyledMenuItem,
+  StyledSpan,
+  StyledList,
+} from '../styles/StyledSidebar';
 
 const Sidebar = () => {
   const [sidebar, setSidebar] = useState(true);
---
+++ src/styles/StyledSidebar.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const StyledSidebar = styled.div`
+  background-color: #4f4a41;
+  overflow: auto;
+  height: auto;
+  padding: 0.5rem;
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: none;
+  width: 50px;
+`;
+
+export const StyledSpan = styled.span`
+  margin-left: 20px;
+`;
+
+export const StyledMenuItem = styled.ul`
+  width: 100%;
+  padding: 1px;
+`;
+
+export const StyledList = styled.li`
+  cursor: pointer;
+`;
+
+// export const StyledNavItem = styled.a`
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+//   padding: 8px 0px 8px 8px;
+//   list-style: none;
+//   height: 50px;
+// `;
+
+export const ToggleButton = styled.button`
+  position: absolute;
+  bottom: 150px;
+`;
